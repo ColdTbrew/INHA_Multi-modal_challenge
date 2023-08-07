b8054d3632885bd24b6e396ba92e800c7f1399ab
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2023-08-07 22:23:56 +0900

    Add files via upload

---
+++ main-BM3.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+# @email: enoche.chow@gmail.com
+
+"""
+Main entry
+# UPDATED: 2022-Feb-15
+##########################
+"""
+
+import os
+import argparse
+from utils.quick_start import quick_start
+os.environ['NUMEXPR_MAX_THREADS'] = '48'
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', '-m', type=str, default='BM3', help='name of models')    #모델 지정
+    parser.add_argument('--dataset', '-d', type=str, default='dacon', help='name of datasets')  #데이터셋 지정
+
+    config_dict = {
+        'gpu_id': 2
+    }
+
+    args, _ = parser.parse_known_args()
+
+    quick_start(model=args.model, dataset=args.dataset, config_dict=config_dict, save_model=True)
+
+
